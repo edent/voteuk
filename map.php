2a4c890db127d328f16d8c504040db0a0763ba0c
terence.eden <terence.eden@c3bdb5b4-2374-11df-9830-2f28089abc60>
2010-02-28 16:30:12 +0000

    Proper Google Maps, TWFY image sizes, rationalisation

--- map.php
+++ map.php
@@ -1,33 +1,25 @@
 <?php 
-/*
-	try
-	{
-		$deviceScreenWidth = Mobi_Mtld_DA_Api::getPropertyAsInteger($tree, $ua, "displayWidth");
-		$deviceScreenHeight = Mobi_Mtld_DA_Api::getPropertyAsInteger($tree, $ua, "displayHeight");
-	}
-	catch (Mobi_Mtld_Da_Exception_InvalidPropertyException $e)
+	//Use DeviceAtlas to get the usable screen size of the device
+	$deviceScreenWidth = $device->usableDisplayWidth;
+	$deviceScreenHeight = $device->usableDisplayHeight;
+	
+	//If we can't get a usable screen size, default to 240*240
+	if ($deviceScreenWidth == null)
 	{
 		$deviceScreenWidth = 240;
 		$deviceScreenHeight = 240;
-		echo "<!-- DEBUG: DeviceAtlas didn't find a match. Using default Width &amp; Height<br/> -->";
 	}
-	*/
-	$deviceScreenWidth = $device->usableDisplayWidth;
-	$deviceScreenHeight = $device->usableDisplayHeight;
-		
+	
+	//Sanitise the postcode data	
 	$safePostcode = str_replace(" ", "+", $postcodeGet);
 	$safePostcodePoll = str_replace(" ", "+", $council_postcode);
-	/*
-	<img src="<?php echo googleMap($safePostcode, $deviceScreenWidth, $deviceScreenHeight); ?>" />
-	<img src="<?php echo yahooMap($safePostcode, $deviceScreenWidth, $deviceScreenHeight); ?>" />
-	<img src="<?php echo ernestmarples($safePostcode, $deviceScreenWidth, $deviceScreenHeight); ?>" />
-	*/
+
 ?>
 This map shows your home and the council offices.<br/>
 <a href="http://maps.google.co.uk/m?q=<?php echo $safePostcodePoll; ?>&amp;site=maps&amp;sa=X&amp;oi=map&amp;ct=res">
 	<?php 
+	
 		$map_img = mashupMapHomePoll($safePostcode, $safePostcodePoll, $deviceScreenWidth, $deviceScreenHeight); 
-		echo "<img src='{$map_img}' alt='A map of your house and council offices' />";	
+		echo "<img src='{$map_img}' width='{$deviceScreenWidth}' height='{$deviceScreenHeight}' alt='A map of your house and council offices' />";	
 	?>
 </a>
-

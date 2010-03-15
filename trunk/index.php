c95ff3ef45426efffa713c5c80ed3d85c2ebaf81
terence.eden <terence.eden@c3bdb5b4-2374-11df-9830-2f28089abc60>
2010-03-15 18:55:29 +0000

    Placed Call To Action (form) at the top on main page

--- trunk/index.php
+++ trunk/index.php
@@ -3,6 +3,9 @@
 	session_start();
 	include "dtd.xhtml-mp.php";
 	include "functions.php";
+
+	$postcodeGet = $_GET["q"];
+
 ?>
 	<head>
 		<?php
@@ -14,6 +17,10 @@
 		<div class="header">
 			<h1>It's time for you to make a difference</h1>
 		</div>
+		<div class="content">
+			<h2>You only have <?php echo (daysUntilElection() - 11); ?> days left to register to vote!</h2>
+		</div>
+
 <?php
 	$postcodeGet = $_GET["q"];
 	if ($postcodeGet)
@@ -45,29 +52,30 @@
 	else
 	{
 ?>	 
-		<div class="content">
-			<h2>You only have <?php echo (daysUntilElection() - 11); ?> days left to register to vote!</h2>
-			We're here to help you:
-			<ul>
-				<li>Register to vote</li>
-				<li>See who wants your vote</li>
-				<li>Find your polling station</li>
-			</ul>
-		</div>
+		
 <?php
 	}
 ?>
-		<div class="form">
+	<div class="form">
 			<form method="get" action="/">
 				<p>
 					<label for="q">What's your <em>full home</em> postcode? For example W1A 1AA.</label><br/>
-					<input class="input" type="text" title="Enter your postcode" maxlength="8" size="8" name="q" id="q" value="" />
+					<input class="input" type="text" title="Enter your postcode" maxlength="8" name="q" id="q" value="" />
+					<br />
 					<input type="submit" value="Help me vote" /> 
 				</p>
 			</form>		
 		</div>
 		<div class="content">
 			We will never pass on your postcode or personal details to any political party.  This site is anonymous.<br/>
+		</div>
+		<div class="content">
+			We're here to help you:
+			<ul>
+				<li>Register to vote</li>
+				<li>See who wants your vote</li>
+				<li>Find your polling station</li>
+			</ul>
 		</div>
 		<div class="content">
 			<a href="mailto:ivote2010.co.uk@shkspr.mobi">Email your feedback, comments and suggestions</a>.<br/>
@@ -80,4 +88,3 @@
 		</div>
 	</body>
 </html>
-
